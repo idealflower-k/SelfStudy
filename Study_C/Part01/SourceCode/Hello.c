003ea5848eab2de1138933cae9598ab6e7c070d6
idealflower-k <aekrcu@naver.com>
2021-12-06 19:08:33 +0900

    chapter01-1,2 / 02-1

---
+++ Study_C/Part01/SourceCode/Hello.c
@@ -0,0 +1,22 @@
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    return (0);
+}
+*/
+
+// P.39 문제 02-1
+#include <stdio.h>
+int main(void)
+{
+    printf("이상화\n이 상 화\n이  상  화\n");
+
+    printf("이름: 이상화\n");
+    printf("주소: 서울특별시 금천구 독산로18길 28 (첼시하우스2, B동, 403호)\n");
+    printf("전화번호: 010-8680-8048");
+
+    return (0);
+}
