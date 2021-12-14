d530b1d3c54c217bd56a6ab30946419033158af8
idealflower-k <aekrcu@naver.com>
2021-12-14 20:53:11 +0900

    exam_C

--- Study_C/Part01/SourceCode/Test.c
+++ Study_C/Part01/SourceCode/Test.c
@@ -2,15 +2,10 @@
 
 int main(void)
 {
-    int total = 0, num = 0;
-    
-    do
-    {
-        printf("정수 입력 (0 종료): ");
-        scanf("%d", &num);
-        total += num;
-    } while (num != 0);
-    printf("합계: %d\n", total);
+    char ch;
+    printf("입력: ");
+    scanf(" %c", &ch);
 
+    printf("%c", ch);
     return (0);
 }
