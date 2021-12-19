04f169febd68e8aaeade0a2dba239564bc9fae09
idealflower-k <aekrcu@naver.com>
2021-12-19 19:49:30 +0900

    ch09

--- Study_C/Part01/SourceCode/Test.c
+++ Study_C/Part01/SourceCode/Test.c
@@ -1,39 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int AbsoCompare(int num1, int num2);
-int GetAbsoValue(int num);
+
+int Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
 
 int main(void)
 {
-    int num1, num2;
-
-    printf("두 개의 정수 입력: ");
-    scanf("%d %d", &num1, &num2);
-    printf("%d와 %d중 절대값이 큰 정수: %d \n", num1, num2, AbsoCompare(num1, num2));
+    printf("1! = %d\n", Factorial(1));
+    printf("2! = %d\n", Factorial(2));
+    printf("3! = %d\n", Factorial(3));
+    printf("4! = %d\n", Factorial(4));
+    printf("9! = %d\n", Factorial(9));
 
     return (0);
-}
-
-int AbsoCompare(int num1, int num2)
-{
-    if (GetAbsoValue(num1) > GetAbsoValue(num2))
-    {
-        return num1;
-    }
-    else
-    {
-        return num2;
-    }
-}
-
-int GetAbsoValue(int num)
-{
-    if (num < 0)
-    {
-        return num * -1;
-    }
-    else
-    {
-        return num;
-    }
 }
--- Study_C/Part01/SourceCode/ch09-1_Func.c
+++ Study_C/Part01/SourceCode/ch09-1_Func.c
@@ -19,32 +19,24 @@
 
 // int GetBigOne(int num1, int num2, int num3)
 // {
-//     if (num1 > num2 && num1 > num3)
+//     if (num1 > num2)
 //     {
-//         return num1;
-//     }
-//     else if (num2 > num1 && num2 > num3)
-//     {
-//         return num2;
+//         return (num1 > num3) ? num1 : num3;
 //     }
 //     else
 //     {
-//         return num3;
+//         return (num2 > num3) ? num2 : num3;
 //     }
 // }
 // int GetSmallOne(int num1, int num2, int num3)
 // {
-//     if (num1 < num2 && num1 < num3)
+//     if (num1 < num2)
 //     {
-//         return num1;
-//     }
-//     else if (num2 < num1 && num2 < num3)
-//     {
-//         return num2;
+//         return (num1 < num3) ? num1 : num3;
 //     }
 //     else
 //     {
-//         return num3;
+//         return (num2 < num3) ? num2 : num3;
 //     }
 // }
 
